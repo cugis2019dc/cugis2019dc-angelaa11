98042e1fed16b33c2a16611b8aad8a233d2e9c42
angelaa11 <53440596+angelaa11@users.noreply.github.com>
2019-07-31 10:11:44 -0400

    Add files via upload

---
+++ Phython Functions.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print("hello")
+
+import plotly
+
+dir(plotly)
+print("I am Angela")
+print(5*2)
+print(5/2)
+print(5-2)
+print(5**2)
+print(8/9*3)
+print("5*2")
+
+def multiply(a,b):
+    multiply=a*b
+
+def sum(a,b):
+    sum(a+b)
+    print(sum)
+
+def area(a,b,c):
+    area=.5*13*24
+    print(area)
+    
+def triArea(b,h):
+    triArea=.5*b*h
+    print("the area of the triangle with a base of ,'b', and a height of 'h, 'is', triArea)
+    
+triArea(1,2)
+
+
+ tree = q +15
+ print(tree)
+ 
+def cadburychocoalte1 (c,a,b):
+    cadburychocoalte1 = (c+a+b)
+    print(cadburychocoalte1)
+    
+    
+ def cadburychocolate (c,a,b):
+     print(c,a,b)
+     cadburychocolate('c','a','b')
+     
+print("Hello Kaya")
+
+def hello(a):
+    a= "Kaya"
+    print("hello",a)
+    
+name= input("please enter your name")
+print("Your name is", name)
+
+age = input("please enter your age")
+print("Thank you, you entered", age)
+
+import math
+
+dir(math)
+
+math.pi
+
+math.pow(12,8)
+
+
+value= input("Please enter a number")
+"value of variable"
+print("Please enter a value for your variable")
+
+import math
+a= int(input)("enter a value"))
+
+def cadbury(a):
+    cadbury=math.pow(a.1/3)
+    print("the cubic root of", a, "is", cadbury)
+    
+cadbury(a)
